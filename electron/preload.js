b1be907958177eae4ca8889585a1a1ecc9a2a388
N1ghthill <irving@ruas.dev.br>
2026-03-17 05:47:05 -0300

    chore: remove legacy electron artifacts

--- electron/preload.js
+++
@@ -1,8 +0,0 @@
-const { contextBridge, ipcRenderer } = require('electron');
-
-// API segura disponível para o renderer.
-contextBridge.exposeInMainWorld('merlinAPI', {
-  ask: (question) => ipcRenderer.invoke('ask-merlin', question),
-  getDocuments: () => ipcRenderer.invoke('get-documents'),
-  onUpdateAvailable: (callback) => ipcRenderer.on('update-available', callback),
-});
